ead4452bf4ca395b206e661444a2a8c343fc83c7
Johan Zambrano <ejzambrano4@espe.edu.ec>
2020-02-12 16:54:34 -0500

    Tutorial terminado

--- src/main.js
+++ src/main.js
@@ -3,11 +3,48 @@ import App from "./App.vue";
 import router from "./router";
 import AuthPlugin from "./plugins/auth";
 
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(VueApollo);
 Vue.use(AuthPlugin);
 
 Vue.config.productionTip = false;
 
+const getHeaders = () => {
+  const headers = {};
+  const token = window.localStorage.getItem("apollo-token");
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+  return headers;
+};
+// Create a WebSocket link:
+const link = new WebSocketLink({
+  uri: "wss://hasura.io/learn/graphql",
+  options: {
+    reconnect: true,
+    timeout: 30000,
+    connectionParams: () => {
+      return { headers: getHeaders() };
+    }
+  }
+});
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
+const apolloProvider = new VueApollo({
+  defaultClient: client
+});
+
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
